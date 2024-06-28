4015a9adf10238e0f92a5e74dcd38741eb552398
indialifah <indialifah25@gmail.com>
2024-06-28 16:39:28 +0700

    tidy up components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar.jsx'
 import Jumbotron from '../components/Jumbotron.jsx'
 import Strength from '../components/Strength.jsx'
 import Agents from '../components/Agents.jsx'
-import ArtikelContent from '../components/ArtikelContent.jsx'
+import ArtikelHome from '../components/ArtikelHome.jsx'
 import Footer from '../components/Footer.jsx'
 import ScrollButton from '../components/ScrollButton.jsx'
 import Contact from '../components/Contact.jsx'
@@ -15,7 +15,7 @@ const Home = () => {
         <Jumbotron />
         <Strength />
         <Agents />
-        <ArtikelContent />
+        <ArtikelHome />
         <Contact />
         <Footer />
         <ScrollButton />
